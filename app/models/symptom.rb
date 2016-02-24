f155e0b2c98872883d05735bb431a4e84792baeb
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-02-24 14:46:03 -0800

    added specs for symptom model

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,3 +1,5 @@
 class Symptom < ActiveRecord::Base
   belongs_to :users
+  validates :name, presence: true
+  validates :start_time, presence: true
 end
--- spec/models/symptom_spec.rb
+++ spec/models/symptom_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Symptom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    let(:good_symptom) do
+      Symptom.create({
+      name: "happiness",
+      start_time: Time.now
+      })
+    end
+
+    let(:bad_symptom) do
+      Symptom.new
+    end
+
+    it "requires an symptom to have a name" do
+      expect(bad_symptom).to be_invalid
+      expect(bad_symptom.errors.keys).to include :name
+    end
+
+    it "requires an symptom to have a start time" do
+      expect(bad_symptom).to be_invalid
+      expect(bad_symptom.errors.keys).to include :start_time
+    end
+  end
 end
