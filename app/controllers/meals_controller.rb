bc8913cc7c733edd84205b31482d3f8183baddbd
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-02-25 11:55:05 -0800

    user can now add and see meals

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,27 @@
+class MealsController < ApplicationController
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params[:meal])
+    if @meal.save
+      redirect_to meal_path(@meal)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    id = params[:id]
+    @meal = Meal.find(id)
+  end
+
+  private
+
+  def meal_params
+    params.permit(meal:[:name, :foods_list, :ingredients_list, :category])
+  end
+
+end
