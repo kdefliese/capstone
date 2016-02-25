9b24d907fa9f9bfa2c090f2968ee99f5a50eaea0
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-02-25 11:18:08 -0800

    added before action for requiring login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    unless current_user
+      flash[:error] = "Please log in to view this section."
+      redirect_to login_path
+    end
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
+  before_action :require_login, except: [:login]
   include FactualWrapper
+
+
+  def login
+  end
 
   def index
     @results = get_product
