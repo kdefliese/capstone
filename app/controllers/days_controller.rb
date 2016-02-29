f15ebd6b29a56816e58209ca854a7f2eff0b5985
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-02-29 11:24:07 -0800

    added some new fields to the food entry form

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -4,6 +4,10 @@ class DaysController < ApplicationController
     @day = Day.find(params[:id])
     @entries = @day.entries
     @entry = Entry.new
+    @food = Food.new
+    @all_meals = Meal.all
+    @all_foods = Food.all
+    @all_ingredients = Ingredient.all
   end
 
 end
