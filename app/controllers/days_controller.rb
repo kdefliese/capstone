fc910d0094376aca2104688dba9337f237a35619
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-02-29 12:40:02 -0800

    working on getting second dropdown to dynamically populate with values based on first dropdown

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,13 +1,18 @@
 class DaysController < ApplicationController
+  before_action :current_user
 
   def show
     @day = Day.find(params[:id])
     @entries = @day.entries
     @entry = Entry.new
     @food = Food.new
-    @all_meals = Meal.all
+    @all_meals = @current_user.meals
     @all_foods = Food.all
     @all_ingredients = Ingredient.all
+    @select_arr = []
+    @all_meals.each do |meal|
+      @select_arr.push("<option value=\"#{meal.id}\">#{meal.name}</option>")
+    end
   end
 
 end
