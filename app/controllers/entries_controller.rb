d4c3682d16dcb2dc39646437650c5bd672d7e8e2
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-02-29 22:13:17 -0800

    tried to associate meal with entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,7 +2,9 @@ class EntriesController < ApplicationController
 
   def create
     @entry = Entry.new(entry_params)
+    binding.pry
     if @entry.save
+      @entry.meals << Meal.find(entry_params[:meal_id])
       redirect_to day_path(Day.find(1))
     else
 
