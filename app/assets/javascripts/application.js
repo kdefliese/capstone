fbd2a77fe0c4eb5ac2bf1f25b89d44b7a597db73
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-03-01 16:23:54 -0800

    added hardcoded autocomplete to day show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -129,4 +129,34 @@ $(document).on('ready', function() {
     console.log("click");
   });
 
+  // autocomplete for search
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $("#tags").autocomplete({
+      source: availableTags
+    });
+
+
 });
