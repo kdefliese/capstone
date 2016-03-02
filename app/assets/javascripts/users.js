44ec5789ca3eeae30db6c9628aaa6dd51a2d223a
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-03-02 15:48:51 -0800

    added prevent defaults for buttons on edit profile page

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,14 @@
+$(document).on('ready', function() {
+  $("#add-food-intolerance").click(function() {
+    event.preventDefault();
+  });
+
+  $("#add-watch-food").click(function() {
+    event.preventDefault();
+  });
+
+  $("#add-medical-disorder").click(function() {
+    event.preventDefault();
+  });
+
+});
