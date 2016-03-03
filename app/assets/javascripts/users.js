f4906f90426e83fa22f0cd77938011db9c348935
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-03-02 17:58:58 -0800

    user profile array fields now added to page dynamically, need to save to db

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,14 +1,17 @@
 $(document).on('ready', function() {
   $("#add-food-intolerance").click(function() {
     event.preventDefault();
+    $("#food-intolerances").append("<p>" + $("#new-food-intolerance").val() + "</p>");
   });
 
   $("#add-watch-food").click(function() {
     event.preventDefault();
+    $("#food-watches").append("<p>" + $("#new-food-watch").val() + "</p>");
   });
 
   $("#add-medical-disorder").click(function() {
     event.preventDefault();
+    $("#medical-disorders").append("<p>" + $("#new-medical-disorder").val() + "</p>");
   });
 
 });
