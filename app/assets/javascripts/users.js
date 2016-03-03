95fb097c59eb273324f886b479d010a2ed78c239
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-03-02 22:00:54 -0800

    first of three array fields saves properly to db

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 $(document).on('ready', function() {
   $("#add-food-intolerance").click(function() {
     event.preventDefault();
-    $("#food-intolerances").append("<p>" + $("#new-food-intolerance").val() + "</p>");
+    $("#food-intolerances").append("<p class=\"food-intolerance\">" + $("#new-food-intolerance").val() + "</p>");
   });
 
   $("#add-watch-food").click(function() {
@@ -12,6 +12,39 @@ $(document).on('ready', function() {
   $("#add-medical-disorder").click(function() {
     event.preventDefault();
     $("#medical-disorders").append("<p>" + $("#new-medical-disorder").val() + "</p>");
+  });
+
+  // when they click the update user button
+  $(".submit").click(function() {
+    event.preventDefault();
+    var intolerances = [];
+    var watching = [];
+    var medical = [];
+    $(".food-intolerance").each(function() {
+      intolerances.push($(this).text());
+    });
+    $(".food-watch").each(function() {
+      watching.push($(this).text());
+    });
+    $(".medical").each(function() {
+      medical.push($(this).text());
+    });
+    var int = JSON.stringify(intolerances);
+    var wat = JSON.stringify(watching);
+    var med = JSON.stringify(medical);
+    var patchUrl = "/users/" + $("#user-id").val();
+    $.ajax({
+      method: "PATCH",
+      url: patchUrl,
+      data: { email: $("#user_email").val(), phone: $("#user_phone").val(), name: $("#user_name").val(), image: $("#user_image").val(), notifications_preference: $("#user_notifications_preference").val(), known_intolerances: intolerances }
+      // watching: JSON.stringify(intolerances), medical_disorders: JSON.stringify(intolerances)
+    })
+    .done(function() {
+      console.log("user update success");
+    })
+    .fail(function() {
+      console.log("user update failure");
+    });
   });
 
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,9 @@ class UsersController < ApplicationController
 
   def update
     # need to do something with the three array fields here so they'll update properly
+    binding.pry
     @user = User.update(params[:id], user_params)
-    if @user.save
-      redirect_to user_path
-    else
-      render :edit
-    end
+    redirect_to user_path
   end
 
   def stats
@@ -25,7 +22,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :phone, :name, :image, :known_intolerances, :watching, :medical_disorders, :notifications_preference)
+    params.permit(:id, :email, :phone, :name, :image, :notifications_preference, known_intolerances: [])
   end
 
 end
