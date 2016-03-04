f413f69c64a972901c35dd4104fd64e18f906409
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-03-03 22:17:43 -0800

    added spec for days controller

--- spec/controllers/days_controller_spec.rb
+++ spec/controllers/days_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe DaysController, type: :controller do
+  let(:day) do
+    Day.create(day: DateTime.new(2016,03,03), user_id: 1)
+  end
+
+  let(:good_params) do
+    { user: {
+        name: "kdefliese",
+        email: "kdefliese@gmail.com",
+        uid: "11111111",
+        provider: "test",
+        notifications_preference: true}
+    }
+  end
+
+
+  describe "GET 'show'" do
+    it "renders the show day page" do
+      user = User.create(good_params[:user])
+      session[:user_id] = user.id
+      get :show, id: day.id
+      expect(subject).to render_template :show
+    end
+  end
 
 end
