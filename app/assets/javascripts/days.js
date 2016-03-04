2def9316b0fc2546acb14b04f449b83094b9e3b2
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-03-03 16:10:28 -0800

    symptoms are being created in the db

--- app/assets/javascripts/days.js
+++ app/assets/javascripts/days.js
@@ -165,20 +165,20 @@ $(document).on('ready', function() {
       $.ajax({
         method: "POST",
         url: "/symptoms",
-        data: { user_id: $("#user-id").val(), day_id: $("#day-id").val() }
+        data: { name: $("#symptom-name").val(), severity: $("#symptom-severity").val(), start_time: $("#symptom-start-time").val(), end_time: $("#symptom-end-time").val(), notes: $("#symptom-notes").val(), user_id: $("#user-id").val(), day_id: $("#day-id").val() }
       })
       .done(function() {
         console.log("post symptom success");
           // make another call to get the most recent symptom and add it to the page
-          $.ajax("/symptoms/last")
-            .done(function(data) {
-              console.log("last symptom success");
-              $("#added-symptoms").append(
-              );
-            })
-            .fail(function() {
-              console.log("last symptom failure");
-            });
+          // $.ajax("/symptoms/last")
+          //   .done(function(data) {
+          //     console.log("last symptom success");
+          //     $("#added-symptoms").append(
+          //     );
+          //   })
+          //   .fail(function() {
+          //     console.log("last symptom failure");
+          //   });
           })
       .fail(function() {
         console.log("post symptom failure");
--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -1,6 +1,22 @@
 class SymptomsController < ApplicationController
 
   def create
+    date = Day.find(params[:day_id]).day.to_s.slice(0,10)
+    symptom_start_time = params[:start_time]
+    symptom_start_datetime = DateTime.parse(date + " " + symptom_start_time)
+    symptom_end_time = params[:end_time]
+    symptom_end_datetime = DateTime.parse(date + " " + symptom_end_time)
+    @symptom = Symptom.new(symptom_params)
+    @symptom.start_time = symptom_start_datetime
+    @symptom.end_time = symptom_end_datetime
+    if @symptom.save
+      redirect_to day_path(Day.find(params[:day_id]))
+    end
   end
-  
+
+  private
+
+  def symptom_params
+    params.permit(:name, :severity, :start_time, :end_time, :notes, :user_id, :day_id)
+  end
 end
