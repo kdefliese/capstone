f06adaf66249318d4baaae02c04414e01b69e016
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-03-03 22:38:46 -0800

    added specs for ingredients controller

--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe IngredientsController, type: :controller do
+  let(:good_params) do
+    {ingredient: {
+      name: "Flour",
+      sensitivity_groups: ["gluten"]
+      }
+    }
+  end
 
+  let(:bad_params) do
+    {ingredient: {
+      }
+    }
+  end
+
+  describe "GET 'new'" do
+    it "is successful" do
+      get :new
+      expect(response.status).to eq 200
+      expect(subject).to render_template :new
+    end
+  end
+
+  describe "POST 'create'" do
+    it "successful create redirects to show page" do
+      post :create, good_params
+      expect(Ingredient.all.length).to eq 1
+      expect(response.status).to eq 302
+      expect(subject).to redirect_to ingredient_path(Ingredient.last)
+    end
+    it "unsuccessful create renders new page" do
+      post :create, bad_params
+      expect(Ingredient.all.length).to eq 0
+      expect(response.status).to eq 200
+      expect(subject).to render_template :new
+    end
+  end
+
+  describe "GET 'show'" do
+    it "is successful" do
+      ingredient = Ingredient.create(good_params[:ingredient])
+      get :show, id: ingredient.id
+      expect(response.status).to eq 200
+      expect(subject).to render_template :show
+    end
+  end
+
+  describe "GET 'all'" do
+    it "is successful" do
+      get :all
+      expect(response.response_code).to eq 200
+    end
+
+    it "returns json" do
+      get :all
+      expect(response.header['Content-Type']).to include 'application/json'
+    end
+  end
 end
