5173cb37035cfb0e252c49a655ddc05f4053facf
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-03-06 21:25:44 -0800

    dried up stats endpoint method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,69 +23,16 @@ class UsersController < ApplicationController
   def stats_endpoint
     @symptoms = @current_user.symptoms
     @symptom_arr = []
-    pain = ["Pain",[]]
-    gas = ["Gas",[]]
-    diarrhea = ["Diarrhea",[]]
-    constipation = ["Constipation",[]]
-    headache = ["Headache/Migraine",[]]
-    bloating = ["Bloating",[]]
-    nausea = ["Nausea",[]]
-    heartburn = ["Heartburn",[]]
-    burping = ["Burping",[]]
-    skin = ["Skin problems",[]]
-    @symptoms.each do |s|
-      if s.name == "Pain"
-        pain[1].push([s.start_time,s.severity])
-      elsif s.name == "Gas"
-        gas[1].push([s.start_time,s.severity])
-      elsif s.name == "Diarrhea"
-        diarrhea[1].push([s.start_time,s.severity])
-      elsif s.name == "Constipation"
-        constipation[1].push([s.start_time,s.severity])
-      elsif s.name == "Headache/Migraine"
-        headache[1].push([s.start_time,s.severity])
-      elsif s.name == "Bloating"
-        bloating[1].push([s.start_time,s.severity])
-      elsif s.name == "Nausea"
-        nausea[1].push([s.start_time,s.severity])
-      elsif s.name == "Heartburn"
-        heartburn[1].push([s.start_time,s.severity])
-      elsif s.name == "Burping"
-        burping[1].push([s.start_time,s.severity])
-      elsif s.name == "Skin Problems"
-        skin[1].push([s.start_time,s.severity])
-      end
-    end
-    if pain[1].length > 0
-      @symptom_arr.push(pain)
-    end
-    if gas[1].length > 0
-      @symptom_arr.push(gas)
-    end
-    if diarrhea[1].length > 0
-      @symptom_arr.push(diarrhea)
-    end
-    if constipation[1].length > 0
-      @symptom_arr.push(constipation)
-    end
-    if headache[1].length > 0
-      @symptom_arr.push(headache)
-    end
-    if bloating[1].length > 0
-      @symptom_arr.push(bloating)
-    end
-    if nausea[1].length > 0
-      @symptom_arr.push(nausea)
-    end
-    if heartburn[1].length > 0
-      @symptom_arr.push(heartburn)
-    end
-    if burping[1].length > 0
-      @symptom_arr.push(burping)
-    end
-    if skin[1].length > 0
-      @symptom_arr.push(skin)
-    end
+    make_stats_array("Pain")
+    make_stats_array("Gas")
+    make_stats_array("Diarrhea")
+    make_stats_array("Constipation")
+    make_stats_array("Headache/Migraine")
+    make_stats_array("Bloating")
+    make_stats_array("Nausea")
+    make_stats_array("Heartburn")
+    make_stats_array("Burping")
+    make_stats_array("Skin problems")
     render :json => @symptom_arr.as_json, :status => :ok
   end
 
