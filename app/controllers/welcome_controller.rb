319ac8528d25d3446680777501f70899fa56e9b4
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-03-08 20:47:28 -0800

    updated the about page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
-  before_action :require_login, except: [:login]
+  before_action :require_login, except: [:login, :about]
   before_action :current_user, only: [:index]
-  before_action :current_day_for_user, except: [:login]
+  before_action :current_day_for_user, except: [:login, :about]
   include FactualWrapper
 
 
