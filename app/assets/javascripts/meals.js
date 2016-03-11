c5c9ee0847e87518ad000abb42ac67a5842da482
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-03-11 14:58:40 -0800

    added success alert when meals are saved

--- app/assets/javascripts/meals.js
+++ app/assets/javascripts/meals.js
@@ -104,6 +104,7 @@ $(document).on('ready', function() {
           $.ajax("/meals/last")
             .done(function(data) {
               console.log("last meal success");
+              $(".new-meal-success").html("<div class=\"alert alert-success alert-dismissible\" role=\"alert\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>" + data.meal.name + " added!</div>");
               // var foods = "";
               // var ingredients = "";
               // for (var j = 0; j < data.foods.length; j++) {
