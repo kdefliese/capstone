029ca1eeebf2d9ced537f2b86935489ae279e4d1
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-03-17 11:30:11 -0700

    days before and after are generated on day show page

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -9,27 +9,29 @@ class DaysController < ApplicationController
     if @day.user_id != @current_user.id
       redirect_to day_path(@current_day.id)
     else
-      day_before = @day.day - 1
-      check_day_before = @current_user.days.where("day = ?, user_id = ?", day_before, @current_user.id)
+      day_before = @day.day - 86400
+      check_day_before = @current_user.days.where("day = ? AND user_id = ?", day_before, @current_user.id)
       if check_day_before.empty?
         # create day before for this user
         d = Day.new
-        d.day = @day.day - 1
+        d.day = @day.day - 86400
         d.user_id = @current_user.id
         d.save
       end
-      @day_before = @current_user.days.where("day = ?, user_id = ?", day_before, @current_user.id)
+      days_array = @current_user.days.where("day = ? AND user_id = ?", day_before, @current_user.id)
+      @day_before = days_array[0]
       # now repeat with day after
-      day_after = @day.day + 1
-      check_day_after = @current_user.days.where("day = ?, user_id = ?", day_after, @current_user.id)
+      day_after = @day.day + 86400
+      check_day_after = @current_user.days.where("day = ? AND user_id = ?", day_after, @current_user.id)
       if check_day_after.empty?
         # create day after for this user
         d = Day.new
-        d.day = @day.day + 1
+        d.day = @day.day + 86400
         d.user_id = @current_user.id
         d.save
       end
-      @day_after = @current_user.days.where("day = ?, user_id = ?", day_after, @current_user.id)
+      second_days_array = @current_user.days.where("day = ? AND user_id = ?", day_after, @current_user.id)
+      @day_after = second_days_array[0]
       @day_id = @day.id
       @user_id = @current_user.id
       @entries = @current_user.entries.where("day_id = ?", @day_id).order("time")
