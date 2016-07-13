64522396c4cb9d2236372ecd08727755e26ccf84
Katherine Defliese <kdefliese@gmail.com>
2016-07-12 22:43:00 -0700

    refactored days controller

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -3,44 +3,28 @@ class DaysController < ApplicationController
   before_action :current_day_for_user
 
   def show
-    # what if this day ID doesn't exist yet?
-    @day = Day.find(params[:id])
-    # if this day doesn't belong to this user, redirect to today for this user
-    if @day.user_id != @current_user.id
+    # an oops page would probably be better than redirecting to current day if day doesn't exist...
+    @day = Day.find_by id: params[:id]
+    if @day.nil?
       redirect_to day_path(@current_day.id)
-    else
-      day_before = @day.day - 86400
-      check_day_before = @current_user.days.where("day = ? AND user_id = ?", day_before, @current_user.id)
-      if check_day_before.empty?
-        # create day before for this user
-        d = Day.new
-        d.day = @day.day - 86400
-        d.user_id = @current_user.id
-        d.save
+    end
+    if !@day.nil?
+      # is this the best behavior if the day doesn't belong to the current user?
+      if @day.user_id != @current_user.id
+        redirect_to day_path(@current_day.id)
+      else
+        find_or_create_previous_day
+        find_or_create_next_day
+        @day_id = @day.id
+        @user_id = @current_user.id
+        @entries = @current_user.entries.where("day_id = ?", @day_id).order("time")
+        @entry = Entry.new
+        @all_meals = @current_user.meals.order("name")
+        @all_foods = Food.all
+        @all_ingredients = Ingredient.all
+        @symptom = Symptom.new
+        @symptoms = @current_user.symptoms.where("day_id = ?", @day_id).order("start_time")
       end
-      days_array = @current_user.days.where("day = ? AND user_id = ?", day_before, @current_user.id)
-      @day_before = days_array[0]
-      # now repeat with day after
-      day_after = @day.day + 86400
-      check_day_after = @current_user.days.where("day = ? AND user_id = ?", day_after, @current_user.id)
-      if check_day_after.empty?
-        # create day after for this user
-        d = Day.new
-        d.day = @day.day + 86400
-        d.user_id = @current_user.id
-        d.save
-      end
-      second_days_array = @current_user.days.where("day = ? AND user_id = ?", day_after, @current_user.id)
-      @day_after = second_days_array[0]
-      @day_id = @day.id
-      @user_id = @current_user.id
-      @entries = @current_user.entries.where("day_id = ?", @day_id).order("time")
-      @entry = Entry.new
-      @all_meals = @current_user.meals.order("name")
-      @all_foods = Food.all
-      @all_ingredients = Ingredient.all
-      @symptom = Symptom.new
-      @symptoms = @current_user.symptoms.where("day_id = ?", @day_id).order("start_time")
     end
   end
 
@@ -67,5 +51,22 @@ class DaysController < ApplicationController
     params.permit(:day, :user_id)
   end
 
+  def find_or_create_previous_day
+    day_before = @day.day - 86400
+    check_day_before = @current_user.days.where("day = ? AND user_id = ?", day_before, @current_user.id)
+    if check_day_before.empty?
+      @day.create_day_before(@current_user)
+    end
+    @day_before = @current_user.days.where("day = ? AND user_id = ?", day_before, @current_user.id).first
+  end
+
+  def find_or_create_next_day
+    day_after = @day.day + 86400
+    check_day_after = @current_user.days.where("day = ? AND user_id = ?", day_after, @current_user.id)
+    if check_day_after.empty?
+      @day.create_day_after(@current_user)
+    end
+    @day_after = @current_user.days.where("day = ? AND user_id = ?", day_after, @current_user.id).first
+  end
 
 end
--- app/models/day.rb
+++ app/models/day.rb
@@ -4,4 +4,18 @@ class Day < ActiveRecord::Base
   has_many :entries
   validates :day, presence: true
 
+  def create_day_before(current_user)
+    d = Day.new
+    d.day = self.day - 86400
+    d.user_id = current_user.id
+    d.save
+  end
+
+  def create_day_after(current_user)
+    d = Day.new
+    d.day = self.day + 86400
+    d.user_id = current_user.id
+    d.save
+  end
+
 end
